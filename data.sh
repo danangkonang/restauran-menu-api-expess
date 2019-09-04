986bfe29b7cee9355af2c8aa8acb813c7255bd80
danangkonang <dngrifai21@gmail.com>
2019-09-04 20:22:28 +0700

    repare data.sh

--- data.sh
+++ data.sh
@@ -1,3 +1,4 @@
 node_modules/.bin/sequelize db:migrate:undo:all
 node_modules/.bin/sequelize db:migrate
-node_modules/.bin/sequelize db:seed:all
+node_modules/.bin/sequelize db:seed --seed 20190904011734-demo-categori
+node_modules/.bin/sequelize db:seed --seed 20190904011749-demo-menu
